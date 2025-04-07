d66f3ac3b055a130d9f218c3cff0b42705b0120d
Julian Alvarez <julian.alvarez.caro@hotmail.com>
2025-04-07 00:06:56 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'task_master_fe',
+  webDir: 'dist'
+};
+
+export default config;
